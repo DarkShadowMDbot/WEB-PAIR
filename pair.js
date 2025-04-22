3badb95a466cf1dde4f3e20479f72b98b9d6f09b
DarkShadowMDbot <darkshadowwhbot@gmail.com>
2025-04-22 19:29:13 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -81,7 +81,7 @@ router.get("/", async (req, res) => {
               ""
             );
 
-            const sid = `*ROBIN [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
+            const sid = `*Davin MD v2*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*https://wa.me/94742366672*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/FhILwsMwkJhFDSDxOC6n1T*`;
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
